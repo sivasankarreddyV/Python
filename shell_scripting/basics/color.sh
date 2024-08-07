1d36777e9040f2b3e8d5f5c058db6b35865548e4
EC2 Default User <ec2-user@ip-172-31-47-173.eu-north-1.compute.internal>
2024-08-07 12:00:21 +0000

    colors

---
+++ shell_scripting/basics/color.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+red="\e[31m"
+green="\e[32m"
+yellow="\e[33m"
+normal="\e[0m"
+
+echo -e "${red}hii this is siva ${normal}"
+echo -e "${green}hii this is siva from hyderabad ${normal}"
+echo -e "${yellow}borabanda badsha ${normal}"
+
